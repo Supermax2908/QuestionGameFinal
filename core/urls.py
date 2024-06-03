2c68a1c00684e96c90fe2f228ee56cdb0aac639b
maks byzovskiy <byzovskiy290808@gmail.com>
2024-06-03 12:14:41 +0300

    connect registration to server

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.introduction.urls'))
+    path('', include('apps.introduction.urls')),
+    path('registration/', include('apps.registration.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
